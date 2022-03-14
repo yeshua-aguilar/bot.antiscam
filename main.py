356c3c3524950510439fed2873ddfcf22496bfda
yeshuaaguilar <81573986+yeshua-aguilar@users.noreply.github.com>
2022-03-14 16:38:33 -0500

    1

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+from discord.ext import commands
+from AntiScam import AntiScam
+
+whitelist = [
+    582046574184759296
+]  # Here you can add the IDs of the users allowed to bypass the AntiScam system.
+logs_channel = 950125030602330162  # Here you can add the ID of the channel where the logs will be sent.
+
+bot = commands.Bot(command_prefix='>')
+bot.remove_command('help')
+# Remove this line if you want to use the help command.
+
+
+@bot.listen()
+async def on_message(message):
+    await AntiScam(message,
+                   bot=bot,
+                   whitelist=whitelist,
+                   muted_role='Muted',
+                   verified_role='Verified',
+                   logs_channel=logs_channel)
+
+
+# Here you can change the names of the roles
+
+bot.run(os.environ['TOKEN'])
